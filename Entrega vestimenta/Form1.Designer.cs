bee52b9281732bcb339505ef9c3993c829c31366
Alevs4 <r.alejandrovalencia@gmail.com>
2023-02-21 08:43:50 -0300

    nuevo archivo

--- Entrega vestimenta/Form1.Designer.cs
+++ Entrega vestimenta/Form1.Designer.cs
@@ -34,8 +34,19 @@
             this.panelLogin = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.formRedondo2 = new Entrega_vestimenta.Componentes.FormRedondo();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TxtPass = new System.Windows.Forms.TextBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.formRedondo3 = new Entrega_vestimenta.Componentes.FormRedondo();
+            this.formRedondo4 = new Entrega_vestimenta.Componentes.FormRedondo();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnIngresar = new Entrega_vestimenta.Componentes.BotonesRedondos();
+            this.BtnSalir = new Entrega_vestimenta.Componentes.BotonesRedondos();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // formRedondo1
@@ -59,11 +70,17 @@
             this.panelLogin.BackColor = System.Drawing.Color.Transparent;
             this.panelLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLogin.BackgroundImage")));
             this.panelLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLogin.Controls.Add(this.BtnSalir);
+            this.panelLogin.Controls.Add(this.BtnIngresar);
+            this.panelLogin.Controls.Add(this.label3);
+            this.panelLogin.Controls.Add(this.label2);
+            this.panelLogin.Controls.Add(this.TxtUsuario);
+            this.panelLogin.Controls.Add(this.TxtPass);
             this.panelLogin.Controls.Add(this.label1);
             this.panelLogin.ForeColor = System.Drawing.Color.Transparent;
-            this.panelLogin.Location = new System.Drawing.Point(475, 98);
+            this.panelLogin.Location = new System.Drawing.Point(472, 153);
             this.panelLogin.Name = "panelLogin";
-            this.panelLogin.Size = new System.Drawing.Size(472, 570);
+            this.panelLogin.Size = new System.Drawing.Size(472, 607);
             this.panelLogin.TabIndex = 1;
             // 
             // label1
@@ -82,11 +99,122 @@
             this.formRedondo2.CornerRadius = 30;
             this.formRedondo2.TargetControl = this.panelLogin;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(27, 744);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(137, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // TxtPass
+            // 
+            this.TxtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPass.Location = new System.Drawing.Point(94, 326);
+            this.TxtPass.Name = "TxtPass";
+            this.TxtPass.Size = new System.Drawing.Size(306, 38);
+            this.TxtPass.TabIndex = 1;
+            this.TxtPass.UseSystemPasswordChar = true;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsuario.Location = new System.Drawing.Point(94, 233);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(306, 38);
+            this.TxtUsuario.TabIndex = 2;
+            // 
+            // formRedondo3
+            // 
+            this.formRedondo3.CornerRadius = 30;
+            this.formRedondo3.TargetControl = this.TxtPass;
+            // 
+            // formRedondo4
+            // 
+            this.formRedondo4.CornerRadius = 30;
+            this.formRedondo4.TargetControl = this.TxtUsuario;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(91, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(90, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnIngresar.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnIngresar.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnIngresar.BorderRadius = 20;
+            this.BtnIngresar.BorderSize = 0;
+            this.BtnIngresar.FlatAppearance.BorderSize = 0;
+            this.BtnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresar.ForeColor = System.Drawing.Color.Black;
+            this.BtnIngresar.Location = new System.Drawing.Point(22, 435);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(186, 55);
+            this.BtnIngresar.TabIndex = 5;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.TextColor = System.Drawing.Color.Black;
+            this.BtnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.BackColor = System.Drawing.Color.Silver;
+            this.BtnSalir.BackgroundColor = System.Drawing.Color.Silver;
+            this.BtnSalir.BorderColor = System.Drawing.Color.Silver;
+            this.BtnSalir.BorderRadius = 15;
+            this.BtnSalir.BorderSize = 0;
+            this.BtnSalir.FlatAppearance.BorderSize = 0;
+            this.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSalir.ForeColor = System.Drawing.Color.Black;
+            this.BtnSalir.Location = new System.Drawing.Point(260, 435);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(180, 55);
+            this.BtnSalir.TabIndex = 6;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.TextColor = System.Drawing.Color.Black;
+            this.BtnSalir.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(676, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sistema de Entrega Vestimenta Personal";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1357, 906);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panelLogin);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -96,7 +224,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelLogin.ResumeLayout(false);
             this.panelLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,6 +237,16 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private Componentes.FormRedondo formRedondo2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Componentes.BotonesRedondos BtnSalir;
+        private Componentes.BotonesRedondos BtnIngresar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox TxtPass;
+        private Componentes.FormRedondo formRedondo3;
+        private Componentes.FormRedondo formRedondo4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
