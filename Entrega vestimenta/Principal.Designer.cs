8de161711ee96994405080003b34e0493664420e
Rafael Valencia <informatico@fruttita.cl>
2023-02-27 13:52:39 -0300

    actualizacion 2

--- Entrega vestimenta/Principal.Designer.cs
+++ Entrega vestimenta/Principal.Designer.cs
@@ -29,14 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xVerificationControl = new DPFP.Gui.Verification.VerificationControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.Lbl_NomUsu = new System.Windows.Forms.Label();
             this.lbl_rolNom = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -48,34 +52,70 @@
             this.panel1.Size = new System.Drawing.Size(1515, 77);
             this.panel1.TabIndex = 0;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(348, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(765, 38);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Entrega de Vestimenta e implementos de seguridad";
+            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(41, 171);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 167);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1434, 669);
+            this.tabControl1.Size = new System.Drawing.Size(1450, 654);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.xVerificationControl);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1426, 640);
+            this.tabPage1.Size = new System.Drawing.Size(1442, 621);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Registro entrega Vestimenta";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 29);
+            this.label1.TabIndex = 182;
+            this.label1.Text = "Ingresar Huella";
+            // 
+            // xVerificationControl
+            // 
+            this.xVerificationControl.Active = true;
+            this.xVerificationControl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.xVerificationControl.Location = new System.Drawing.Point(98, 72);
+            this.xVerificationControl.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.xVerificationControl.Name = "xVerificationControl";
+            this.xVerificationControl.ReaderSerialNumber = "00000000-0000-0000-0000-000000000000";
+            this.xVerificationControl.Size = new System.Drawing.Size(46, 49);
+            this.xVerificationControl.TabIndex = 181;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1426, 640);
+            this.tabPage2.Size = new System.Drawing.Size(1442, 621);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Informes de entrega";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Lbl_NomUsu
@@ -84,7 +124,7 @@
             this.Lbl_NomUsu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_NomUsu.Location = new System.Drawing.Point(59, 89);
             this.Lbl_NomUsu.Name = "Lbl_NomUsu";
-            this.Lbl_NomUsu.Size = new System.Drawing.Size(106, 36);
+            this.Lbl_NomUsu.Size = new System.Drawing.Size(85, 29);
             this.Lbl_NomUsu.TabIndex = 2;
             this.Lbl_NomUsu.Text = "label1";
             // 
@@ -94,28 +134,28 @@
             this.lbl_rolNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_rolNom.Location = new System.Drawing.Point(59, 129);
             this.lbl_rolNom.Name = "lbl_rolNom";
-            this.lbl_rolNom.Size = new System.Drawing.Size(106, 36);
+            this.lbl_rolNom.Size = new System.Drawing.Size(85, 29);
             this.lbl_rolNom.TabIndex = 3;
             this.lbl_rolNom.Text = "label2";
             // 
-            // label3
+            // label2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(348, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(798, 39);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Entrega de Vestimenta e implementos de seguridad";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(357, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 36);
+            this.label2.TabIndex = 183;
+            this.label2.Text = "Personal";
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1519, 852);
+            this.ClientSize = new System.Drawing.Size(1526, 852);
             this.Controls.Add(this.lbl_rolNom);
-            this.Controls.Add(this.Lbl_NomUsu);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.Lbl_NomUsu);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Principal";
@@ -125,6 +165,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,5 +181,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label Lbl_NomUsu;
         private System.Windows.Forms.Label lbl_rolNom;
+        private System.Windows.Forms.Label label1;
+        private DPFP.Gui.Verification.VerificationControl xVerificationControl;
+        private System.Windows.Forms.Label label2;
     }
 }
